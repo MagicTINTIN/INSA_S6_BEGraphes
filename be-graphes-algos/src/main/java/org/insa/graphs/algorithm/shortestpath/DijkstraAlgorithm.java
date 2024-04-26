e3d27ad542e067559e9e608ac8d0174accea0c47
MagicTINTIN <valentinservieres@gmail.com>
2024-04-26 17:28:13 +0200

    add labels

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
@@ -1,5 +1,9 @@
 package org.insa.graphs.algorithm.shortestpath;
 
+import org.insa.graphs.algorithm.utils.BinaryHeap;
+import org.insa.graphs.model.Graph;
+import org.insa.graphs.model.Node;
+
 public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 
     public DijkstraAlgorithm(ShortestPathData data) {
@@ -9,8 +13,29 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
     @Override
     protected ShortestPathSolution doRun() {
         final ShortestPathData data = getInputData();
+        final int originID = data.getOrigin().getId();
+        final int destinationID = data.getDestination().getId();
+        boolean isDestinationMarked = false;
         ShortestPathSolution solution = null;
-        // TODO:
+
+        // initialising
+        Graph graph = data.getGraph();
+        final int nbNodes = graph.size();
+        Label nodeLabels[] = new Label[nbNodes];
+        BinaryHeap<Label> heap = new BinaryHeap<>();
+        for (Node node : graph.getNodes()) {
+            nodeLabels[node.getId()] = new Label(node);
+        }
+
+        heap.insert(nodeLabels[originID]);
+        nodeLabels[originID].setCost(0);
+
+        while (!heap.isEmpty() && !isDestinationMarked)
+        {
+            
+        }
+        // for (int i = 0; i < nodeLabels.length; i++) {   
+        // }
         return solution;
     }
 
--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/Label.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/Label.java
@@ -1,8 +1,9 @@
 package org.insa.graphs.algorithm.shortestpath;
 
+import org.insa.graphs.model.Arc;
 import org.insa.graphs.model.Node;
 
-public class Label {
+public class Label implements Comparable<Label> {
     private Node currentVertex;
     private boolean marked;
     private double realisedCost;
@@ -14,5 +15,71 @@ public class Label {
         this.marked = m;
         this.realisedCost = cost;
         this.parent = par;
+    }
+
+    public Label(Node defaultVertex)
+    {
+        this.currentVertex = defaultVertex;
+        this.marked = false;
+        this.realisedCost = Double.MAX_VALUE;
+        this.parent = null;
+    }
+
+    public double getCost()
+    {
+        return this.realisedCost;
+    }
+
+    /**
+     * 
+     * @param newCost
+     * @param parent
+     * @return returns 0 if updated, 1 if cost not updated (new is >), 2 if already marked
+     */
+    public int updateCostAndParent(double newCost, Node newParent)
+    {
+        if (marked)
+            return 2;
+        if (newCost > this.realisedCost)
+            return 1;
+        this.realisedCost = newCost;
+        this.parent = newParent;
+        return 0;
+    }
+
+    public void setCost(double newCost)
+    {
+        this.realisedCost = newCost;
+    }
+
+    public boolean isMarked()
+    {
+        return this.marked;
+    }
+
+    public void mark()
+    {
+        this.marked = true;
+    }
+
+    public void unmark()
+    {
+        this.marked = false;
+    }
+
+    public Node getParent()
+    {
+        return this.parent;
+    }
+
+    public Node getCurrentVertex()
+    {
+        return this.currentVertex;
+    }
+
+
+    @Override
+    public int compareTo(Label o) {
+        return Double.compare(this.realisedCost, o.realisedCost);
     }
 }
