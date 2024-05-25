cdd6edec325f0f4e8fb17f4e9913440f3cd20ad3
MagicTINTIN <valentinservieres@gmail.com>
2024-05-26 00:45:23 +0200

    fix/improve astar time weights

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/LabelStar.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/LabelStar.java
@@ -5,6 +5,8 @@ import org.insa.graphs.model.Node;
 import org.insa.graphs.model.Point;
 
 public class LabelStar extends Label {
+    private static final float coefTemps = 1.2f;
+    private static final float coefVitesseArc = .5f;
     private float shortestDistance;
     
     public LabelStar(Node cVertex, boolean m, boolean r, float cost, Arc par, Node destVertex, int maxSpeed)
@@ -20,14 +22,23 @@ public class LabelStar extends Label {
     }
 
     public float processShortestDistance(Node start, Node end, int maxSpeed) {
-        if (maxSpeed != -1)
-            return (float) Point.distance(start.getPoint(), end.getPoint()) / (1000*maxSpeed);
+        if (maxSpeed != -1) {
+            int maxSpeedArc = 15;
+            for (Arc arc : this.getCurrentVertex().getSuccessors()) {
+                if (arc.getRoadInformation().getMaximumSpeed() > maxSpeedArc)
+                maxSpeedArc = arc.getRoadInformation().getMaximumSpeed();
+            }
+            maxSpeed+=coefVitesseArc*maxSpeedArc+150;
+            //System.out.println("max speed: " + maxSpeed);
+            return (float) Point.distance(start.getPoint(), end.getPoint()) * 3600 / (coefTemps*1000*maxSpeed);
+        }
         return (float) Point.distance(start.getPoint(), end.getPoint());
     }
 
     @Override
     public float getTotalCost()
     {
+        //System.out.println("COSTS : " + this.realisedCost + " + " + shortestDistance);
         return this.realisedCost + shortestDistance;
     }
 }
